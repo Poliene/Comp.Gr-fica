4f20f22b8f00e3c3a2ddbbf13106459023fd30bc
Poliene <poliene.f555@gmail.com>
2024-11-29 23:34:36 -0300

    Update pagina3.lua

--- projetoEbook/pagina3.lua
+++ projetoEbook/pagina3.lua
@@ -38,10 +38,43 @@ function scene:create( event )
         
     end)
 
+    local texto2 = display.newImage(sceneGroup, "/assets/texto2/texto2.png", 287, 492)
+    texto2.x = display.contentWidth - 560
+    texto2.y = display.contentHeight - 560
+
+    local nmr3 = display.newImage(sceneGroup, "/assets/nmr3/nmr3.png", 29, 70)
+    nmr3.x = display.contentWidth - 390
+    nmr3.y = display.contentHeight - 85
+
     local som = display.newImage(sceneGroup, "/assets/botao/som.png")
     som.x = display.contentWidth - som.width/2 - MARGIN - 500
     som.y = display.contentHeight - som.height/2 - MARGIN
+
+    local semsom = display.newImage(sceneGroup, "/assets/botao/semsom.png")
+    semsom.x = display.contentWidth - semsom.width/2 - MARGIN - 500
+    semsom.y = display.contentHeight - semsom.height/2 - MARGIN
+    semsom.isVisible = false  
+
+   
+    local function toggleSound(event)
+        if som.isVisible then
+            som.isVisible = false
+            semsom.isVisible = true
+            
+            print("Som desligado")
+        else
+            som.isVisible = true
+            semsom.isVisible = false
+           
+            print("Som ligado")
+        end
+        return true  
+    end
+
     
+    som:addEventListener("tap", toggleSound)
+    semsom:addEventListener("tap", toggleSound)
+
 end
  
 function scene:show( event )
