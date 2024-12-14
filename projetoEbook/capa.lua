cfc10fbd13f4fb629d4f6b27bd1d76e6a3e95d73
Poliene <poliene.f555@gmail.com>
2024-12-14 19:27:51 -0300

    Update capa.lua

--- projetoEbook/capa.lua
+++ projetoEbook/capa.lua
@@ -1,66 +1,93 @@
 local composer = require("composer")
-local audio = require("audio") 
+local audio = require("audio")
 
 local scene = composer.newScene()
 
 local MARGIN = 40
-local isSoundOn = true 
-
+local isSoundOn = true
+local backgroundSoundHandle
+local clickSoundHandle
 
 local backgroundSound
+local clickSound
+local isSoundPlaying = false
 
 function scene:create(event)
     local sceneGroup = self.view
 
-    
+    audio.setVolume(1)
+
+    local testSound = audio.loadSound("assets/audios/audio1.mp3")
+    if testSound then
+        print("Som de teste carregado com sucesso!")
+        audio.play(testSound)
+    else
+        print("Erro: Não foi possível carregar o som de teste.")
+    end
+
     local background = display.newImageRect(sceneGroup, "assets/capa/capa.png", 768, 1024)
     background.x = display.contentCenterX
     background.y = display.contentCenterY
 
-    
-    local proximo = display.newImage(sceneGroup, "/assets/botao/proximo.png")
+    local proximo = display.newImage(sceneGroup, "assets/botao/proximo.png")
     proximo.x = display.contentWidth - proximo.width / 2 - MARGIN
     proximo.y = display.contentHeight - 940
 
+    local som = display.newImage(sceneGroup, "assets/botao/som.png")
+    som.x = som.width / 2 + MARGIN
+    som.y = display.contentHeight - som.height / 2 - MARGIN
+
+    local semsom = display.newImage(sceneGroup, "assets/botao/semsom.png")
+    semsom.x = som.x
+    semsom.y = som.y
+    semsom.isVisible = false
+
+    local function toggleSound()
+        if isSoundOn then
+            audio.stop()
+            som.isVisible = false
+            semsom.isVisible = true
+            isSoundOn = false
+            print("Som desligado.")
+        else
+            if not backgroundSoundHandle then
+                backgroundSoundHandle = audio.play(backgroundSound, { loops = -1 })
+            end
+            if clickSound then
+                audio.play(clickSound)
+            end
+            som.isVisible = true
+            semsom.isVisible = false
+            isSoundOn = true
+            print("Som ligado.")
+        end
+    end
+
+    som:addEventListener("tap", toggleSound)
+    semsom:addEventListener("tap", toggleSound)
+
+    clickSound = audio.loadSound("assets/audios/audio1.mp3")
+    if not clickSound then
+        print("Erro: Som de clique não foi carregado.")
+    end
+
     proximo:addEventListener("tap", function(event)
+        if backgroundSoundHandle then
+            audio.stop(backgroundSoundHandle)
+            backgroundSoundHandle = nil
+        end
+
+        composer.removeScene("capa")
         composer.gotoScene("pagina2", {
             effect = "fade",
             time = 500
         })
     end)
 
-    
-    local som = display.newImage(sceneGroup, "/assets/botao/som.png")
-    som.x = display.contentWidth - som.width / 2 - MARGIN - 500
-    som.y = display.contentHeight - som.height / 2 - MARGIN
-
-    local semsom = display.newImage(sceneGroup, "/assets/botao/semsom.png")
-    semsom.x = display.contentWidth - semsom.width / 2 - MARGIN - 500
-    semsom.y = display.contentHeight - semsom.height / 2 - MARGIN
-    semsom.isVisible = false 
-
-    
-    backgroundSound = audio.loadStream("audios/audio1.mp3")
-
-    
-    local function toggleSound(event)
-        if som.isVisible then
-            som.isVisible = false
-            semsom.isVisible = true
-            audio.stop() 
-            print("Som desligado")
-        else
-            som.isVisible = true
-            semsom.isVisible = false
-            audio.play(backgroundSound, { loops = -1 }) 
-            print("Som ligado")
-        end
-        return true 
+    backgroundSound = audio.loadStream("assets/audio/background.mp3")
+    if not backgroundSound then
+        print("Erro: Som de fundo não foi carregado.")
     end
-
-    
-    som:addEventListener("tap", toggleSound)
-    semsom:addEventListener("tap", toggleSound)
 end
 
 function scene:show(event)
@@ -68,11 +95,13 @@ function scene:show(event)
     local phase = event.phase
 
     if (phase == "will") then
-       
     elseif (phase == "did") then
-       
-        if isSoundOn then
-            audio.play(backgroundSound, { loops = -1 }) 
+        if isSoundOn and backgroundSound and not isSoundPlaying then
+            backgroundSoundHandle = audio.play(backgroundSound, { loops = -1 })
+            isSoundPlaying = true
+            print("Som de fundo reproduzido.")
+        else
+            print("Som de fundo não disponível ou já está tocando.")
         end
     end
 end
@@ -82,21 +111,30 @@ function scene:hide(event)
     local phase = event.phase
 
     if (phase == "will") then
-        
-        audio.stop() 
+        if backgroundSoundHandle then
+            audio.stop(backgroundSoundHandle)
+            backgroundSoundHandle = nil
+            isSoundPlaying = false
+            print("Som de fundo parado.")
+        end
     elseif (phase == "did") then
-     
     end
 end
 
 function scene:destroy(event)
     local sceneGroup = self.view
 
-    
     if backgroundSound then
-        audio.dispose(backgroundSound) 
+        audio.dispose(backgroundSound)
         backgroundSound = nil
     end
+    if clickSound then
+        audio.dispose(clickSound)
+        clickSound = nil
+    end
+
+    audio.stop()
+    print("Todos os recursos de áudio foram liberados.")
 end
 
 scene:addEventListener("create", scene)
